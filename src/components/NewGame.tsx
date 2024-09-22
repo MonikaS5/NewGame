0233d6d6d3bdf9a675d9480a432750be2e7e2920
MonikaS5 <monika.bhagade@gmail.com>
2024-09-22 11:29:00 +0100

    component NewGame.tsx

---
+++ src/components/NewGame.tsx
@@ -0,0 +1,39 @@
+import { useState } from "react"
+
+
+
+function NewGame() {
+  const [token, setToken] = useState();
+  const [resp, setResp] = useState("");
+  const [form, setForm] = useState({ symbol: "", faction: "COSMIC" });
+
+  return (<>
+    <h1>New Game</h1>
+    <input name="symbol" value={form.symbol} onChange={(e) => setForm({ ...form, symbol: e.currentTarget.value })} />
+    <input name="faction" value={form.faction} onChange={(e) => setForm({ ...form, faction: e.currentTarget.value })} />
+    <input type="submit" onClick={async () => {
+      const resp = await fetch("https://api.spacetraders.io/v2/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          symbol: form.symbol,
+          faction: form.faction,
+        }),
+      });
+
+      const json = await resp.json();
+
+      if (resp.ok) {
+        setToken(json.data.token)
+      }
+
+      setResp(JSON.stringify(json, null, 2))
+    }} />
+    <pre>API token: {token}</pre>
+    <pre>Response: {resp}</pre>
+  </>)
+}
+
+export default NewGame
