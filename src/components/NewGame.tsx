42ac7562651651007a5ff247e1a9d64618939e35
MonikaS5 <monika.bhagade@gmail.com>
2024-09-22 13:27:45 +0100

    handleSubmit: Success and Error message

--- src/components/NewGame.tsx
+++ src/components/NewGame.tsx
@@ -10,6 +10,8 @@ function NewGame() {
   const [resp, setResp] = useState<string>("");
   const [form, setForm] = useState<FormUser>({ symbol: "", faction: "COSMIC" });
   const [isLoading, setIsLoading] = useState<boolean>(false);
+ const [successMessage, setSuccessMessage] = useState<string>("");
+ const [errorMessage, setErrorMessage] = useState<string>("");
 
   const factions = [
     "COSMIC",
@@ -33,8 +35,10 @@ function NewGame() {
   // handleSubmit
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    setIsLoading(true);
 
+    setIsLoading(true);
+    setErrorMessage("");
+    setSuccessMessage("");
 
     try {
       const response = await fetch("https://api.spacetraders.io/v2/register", {
@@ -52,6 +56,9 @@ function NewGame() {
 
       if (response.ok) {
         setToken(json.data.token);
+        setSuccessMessage(`Hello ${form.symbol}, Successfully Registered!`)
+      }else{
+        setErrorMessage(json.error.message || "Registration Failed")
       }
 
       setResp(JSON.stringify(json, null, 2));
@@ -62,10 +69,13 @@ function NewGame() {
     }
   };
 
+  //handleReset
   const handleReset = () => {
     setForm({ symbol: "", faction: "COSMIC" });
     setToken("");
     setResp("");
+    setErrorMessage("");
+    setSuccessMessage("");
   }
 
 
@@ -81,7 +91,7 @@ function NewGame() {
         <div className="row border">
           <div className="col-sm-12 col-md-4">
             <form className="bg-light p-4" onSubmit={handleSubmit}>
-              <div>
+              <div className="mb-3">
                 <label htmlFor="symbol"> Symbol: </label>
                 <input
                   type="text"
@@ -92,10 +102,12 @@ function NewGame() {
                   maxLength={14}
                   minLength={3}
                   onChange={handleChange}
+                  placeholder="Enter Your Symbol"
+                  
                 />
               </div>
               {/* ---input-- */}
-              <div>
+              <div className="mb-3">
                 <label htmlFor="faction"> Faction: </label>
                 <select
                   name="faction"
@@ -123,6 +135,18 @@ function NewGame() {
                 onClick={handleReset}>Reset</button>
               </div>
             </form>
+
+            {successMessage && (
+                <div className="alert alert-success mt-5" role="alert">
+                    <h4 className="alert-heading">Well done!</h4>
+                    {successMessage}
+                    </div>
+            )}
+            {errorMessage && (
+                <div className="alert alert-danger mt-5" role="alert">
+                    {errorMessage}
+                    </div>
+            )}
           </div>
           
           <div className="col-sm-12 col-md-8">
